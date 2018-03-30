cfb795be022bc97408084b8d9a892243f4e0d928
Echo Wang <dongxu@apache.org>
2018-03-30 11:08:28 +0800

    Lesson 2 , task 5.

--- lesson2/task5/src/Task.kt
+++ lesson2/task5/src/Task.kt
@@ -4,12 +4,22 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+class RepeatTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate =
+        addTimeIntervals(timeInterval, 1)
+
+operator fun TimeInterval.times(number: Int): RepeatTimeInterval = RepeatTimeInterval(this, number)
+
+operator fun MyDate.plus(repeatTimeInterval: RepeatTimeInterval): MyDate =
+    addTimeIntervals(repeatTimeInterval.timeInterval, repeatTimeInterval.number)
+
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
