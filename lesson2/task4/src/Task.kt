4db42f2cb170e8fe5acbfaf9abf0a19f621f8dc9
Echo Wang <dongxu@apache.org>
2018-03-30 11:08:15 +0800

    Lesson 2 , task 4.

--- lesson2/task4/src/Task.kt
+++ lesson2/task4/src/Task.kt
@@ -1,4 +1,15 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate): Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = object : Iterator<MyDate> {
+        var current: MyDate = start
+        override fun hasNext(): Boolean = current <= end
+
+        override fun next(): MyDate {
+            val result = current
+            current = current.nextDay()
+            return result
+        }
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
